5547b5c659580956b60d83d6d4a06842341c3dfc
gqueiroz <gribeiro.mg@gmail.com>
2021-01-24 13:04:45 -0300

    Add sits-docker as submmodules
    Use sits-docker as base support for JupyterHub R images

--- build.sh
+++ build.sh
@@ -20,8 +20,14 @@ BUILD_MODE=""
 TAG_PREFIX="bdc"
 TAG_VERSION="1.0.0"
 
-PYTHON_GEOSPATIAL_IMAGE_TAG="${TAG_PREFIX}/pygeo:${TAG_VERSION}"
-R_GEOSPATIAL_IMAGE_TAG="${TAG_PREFIX}/rgeo:${TAG_VERSION}"
+PYTHON_GEOSPATIAL_IMAGE_TAG="${TAG_PREFIX}/jupyterhub-pygeo:${TAG_VERSION}"
+
+R_GEOSPATIAL_BASE1_IMAGE_TAG="${TAG_PREFIX}/jupyterhub-rgeo-base1:${TAG_VERSION}"
+R_GEOSPATIAL_BASE2_IMAGE_TAG="${TAG_PREFIX}/jupyterhub-rgeo-base2:${TAG_VERSION}"
+R_GEOSPATIAL_IMAGE_TAG="${TAG_PREFIX}/jupyterhub-rgeo:${TAG_VERSION}"
+
+SITS_BASE_IMAGE_TAG="${TAG_PREFIX}/jupyterhub-sits-base:${TAG_VERSION}"
+SITS_IMAGE_TAG="${TAG_PREFIX}/jupyterhub-sits:${TAG_VERSION}"
 
 
 #
@@ -41,10 +47,48 @@ cd ../../
 #
 # Build Geospatial R image with all the package dependencies already installed
 #
-#echo "Building R Geospatial image..."
+echo "Building R Geospatial image..."
+echo "\t=>base image 1..."
+
+cd sits-docker/docker/ubuntu
+
+docker build ${BUILD_MODE} \
+       --build-arg BASE_IMAGE=jupyter/minimal-notebook:177037d09156 \
+       -t ${R_GEOSPATIAL_BASE1_IMAGE_TAG} \
+       --file Dockerfile .
+
+echo "\t=>base image 2..."
+
+cd ../R
+
+docker build ${BUILD_MODE} \
+       --build-arg BASE_IMAGE=${R_GEOSPATIAL_BASE1_IMAGE_TAG} \
+       --build-arg SITS_TAG_VERSION=v0.9.8 \
+       --build-arg SITS_ENVIRONMENT_TYPE=full \
+       -t ${R_GEOSPATIAL_BASE2_IMAGE_TAG} \
+       --file Dockerfile  .
+
+cd ../../../docker/rgeo
+
+docker build ${BUILD_MODE} \
+       --build-arg BASE_IMAGE=${R_GEOSPATIAL_BASE2_IMAGE_TAG} \
+       -t ${R_GEOSPATIAL_IMAGE_TAG} \
+       --file Dockerfile  .
+
+
 #
-#cd docker/rgeo
+# Build SITS R image with all the package dependencies already installed
+#
+#cd ../sits
 #
 #docker build ${BUILD_MODE} \
-#       -t ${R_GEOSPATIAL_IMAGE_TAG} \
+#       --build-arg BASE_IMAGE=${R_GEOSPATIAL_IMAGE_TAG} \
+#       -t ${SITS_BASE_IMAGE_TAG} \
+#       --file Dockerfile  .
+#
+#cd ../../../docker/sits
+#
+#docker build ${BUILD_MODE} \
+#       --build-arg BASE_IMAGE=${SITS_BASE_IMAGE_TAG} \
+#       -t ${SITS_IMAGE_TAG} \
 #       --file Dockerfile  .
