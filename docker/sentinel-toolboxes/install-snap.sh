e105a2224d199de2f6cba54c60bda1f8381e08f4
gqueiroz <gribeiro.mg@gmail.com>
2021-06-17 20:36:48 -0300

    Adding Sentinel Toolboxes

---
+++ docker/sentinel-toolboxes/install-snap.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -eou pipefail
+
+#
+# Download (if needed) and install SNAP
+#
+cd /tmp
+
+if [ ! -f /tmp/esa-snap_all_unix_${BDC_SNAP_VERSION}_0.sh ]; then
+
+    wget -q \
+        -O "/tmp/esa-snap_all_unix_${BDC_SNAP_VERSION}_0.sh" \
+        "http://step.esa.int/downloads/${BDC_SNAP_VERSION}.0/installers/esa-snap_all_unix_${BDC_SNAP_VERSION}_0.sh"
+
+    chmod +x esa-snap_all_unix_${BDC_SNAP_VERSION}_0.sh
+fi
+
+./esa-snap_all_unix_${BDC_SNAP_VERSION}_0.sh -q -varfile response.varfile
+
+
+#
+# Download Apache Maven
+#
+wget -q \
+        -O "/tmp/apache-maven-3.8.1-bin.tar.gz" \
+        "https://downloads.apache.org/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.tar.gz"
+
+cd /tmp && tar xzvf apache-maven-3.8.1-bin.tar.gz
+
+
+#
+# Clone and build jpy
+# See: https://forum.step.esa.int/t/unable-to-install-snappy-jpy-problem/5372/15
+#
+git clone https://github.com/bcdev/jpy.git /tmp/jpy
+
+cd /tmp/jpy
+
+PATH=$PATH:/tmp/apache-maven-3.8.1/bin/ JAVA_HOME=/opt/snap/8.0/jre python setup.py bdist_wheel
+
+mkdir -p /opt/snap/8.0/snap-python/snappy/
+
+
+#
+# Copy jpy wheels to snappy folder
+#
+cp dist/*.whl  /opt/snap/8.0/snap-python/snappy/
+
+
+#
+# Configure and build snappy
+#
+/opt/snap/${BDC_SNAP_VERSION}.0/bin/snappy-conf /opt/conda/envs/geospatial/bin/python /opt/snap/${BDC_SNAP_VERSION}.0/snap-python
+
+
+#
+# Install snappy into the conda environment
+#
+cd /opt/snap/${BDC_SNAP_VERSION}.0/snap-python/snappy
+
+python setup.py install
+
+
+#
+# Remove non-necessary software and files
+#
+rm -rf /tmp/apache-maven-3.8.1
+
+rm -rf /tmp/jpy
+
+rm -rf /tmp/esa-snap_all_unix_${BDC_SNAP_VERSION}_0.sh
+
+rm /tmp/response.varfile
