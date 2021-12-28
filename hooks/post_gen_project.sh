619dcae9f101165d50d04d1c5253efa9f0649af3
Phelps <phelpsmemo@gmail.com>
2021-12-28 02:54:07 -0300

    reorganizing the package as a cookiecutter template

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 National Institute for Space Research.
+#
+# BDC JupyterHub Docker is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+echo ""
+echo "Cookiecutter pos-processing..."
+echo ""
+
+mv config/jupyterhub/config-env .env
+
+
+echo "Done!"
