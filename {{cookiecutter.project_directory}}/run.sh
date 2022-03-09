949f72c75f43bc9e498496f3f4c627015a087f5a
Felipe <efelipecarlos@gmail.com>
2022-03-09 02:56:20 -0300

    Update run.sh

--- {{cookiecutter.project_directory}}/run.sh
+++ {{cookiecutter.project_directory}}/run.sh
@@ -6,4 +6,8 @@
 # BDC JupyterHub Docker is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
+{% if cookiecutter.use_nginx == "no" %}
+docker-compose -f docker-compose.yml up -d
+{% else %}
 docker-compose -f docker-compose.yml -f docker-compose.full.yml up -d
+{% endif %}
